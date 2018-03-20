62486227cd8b97e34c6f504f65eca5d345c21a83
ajt-rnk <ajeetvil@gmail.com>
2018-03-20 18:33:23 +0530

    Update Set_33_Zoho.cpp

--- Set_33_Zoho.cpp
+++ Set_33_Zoho.cpp
@@ -37,3 +37,36 @@ for(int i=0;i<s.length(); i++){
 }
 }
 */
+/*
+Q : is Rectangle if it is find sum of nelemnts in rectangle
+
+import java.io.*;
+import java.util.*;
+public class MyClass {
+    static int m,n,sum;
+    public static boolean isRectangle(int x1,int y1,int x2,int y2,int matrix[][]){
+      if(x1==x2 || (y1==0 && y2==m-1)){
+          for(int i=x1;i<=x2;i++){
+              for(int j=y1;j<=y2;j++){
+                  sum+=matrix[i][j];
+              }
+          }
+      return true;
+      }
+      return false;
+    }
+    public static void main(String args[]) throws Exception{
+       Scanner sc=new Scanner(System.in);
+       n=sc.nextInt();
+       m=sc.nextInt();
+       sum=0;
+       int[][] matrix={{1, 2, 3, 4, 6}, {5, 3, 8, 1, 2},
+                       {4, 6, 7, 5, 5}, {2, 4, 8, 9, 4}};
+       if(isRectangle(2,0,3,4,matrix)) System.out.println(sum);
+       /*for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++) System.out.print(matrix[i][j]+" ");
+           System.out.println();
+       }*/
+    }
+}
+*/
